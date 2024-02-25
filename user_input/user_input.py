4b5d1aa12f7cc7f40b98a874423d00d64ae4140c
codedark1 <wellingtonmulima@gmail.com>
2024-02-26 00:24:34 +0300

    first commit

---
+++ user_input/user_input.py
@@ -0,0 +1,5 @@
+name=input('what is your name? ')
+age=input('what is your age? ')
+location=input('where do you come from? ')
+
+print(f'Hello {name}, you are {age}years old and live in {location}')
